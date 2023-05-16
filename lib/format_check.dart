a7489cd0a7374a79db7b8bde334518e4e3f348ac
酸性量子 <lucidquantum@163.com>
2023-05-16 14:46:05 +0800

    static

--- lib/format_check.dart
+++ lib/format_check.dart
@@ -1,14 +1,14 @@
 /// 提供了一些常见格式的检查方法，返回布尔值
 class FormatChecker {
   /// 手机号：11位数字，以1开头
-  bool phone(String phone) {
+  static bool phone(String phone) {
     final RegExp pattern = RegExp(r'^1\d{10}$');
     if (!pattern.hasMatch(phone)) return false;
     return true;
   }
 
   /// 短信验证码：4位数字
-  bool sms(String sms) {
+  static bool sms(String sms) {
     final RegExp pattern = RegExp(r'^\d{4}$');
     if (!pattern.hasMatch(sms)) return false;
     return true;
