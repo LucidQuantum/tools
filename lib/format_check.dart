5968e40c3376db415959b4d835a2ff27cd9f4581
酸性量子 <lucidquantum@163.com>
2023-05-16 15:08:04 +0800

    优化格式检查（自动抛错）

--- lib/format_check.dart
+++ lib/format_check.dart
@@ -1,16 +1,16 @@
+import 'package:tools/refuse.dart';
+
 /// 提供了一些常见格式的检查方法，返回布尔值
 class FormatChecker {
   /// 手机号：11位数字，以1开头
-  static bool phone(String phone) {
+  static void phone(String phone) {
     final RegExp pattern = RegExp(r'^1\d{10}$');
-    if (!pattern.hasMatch(phone)) return false;
-    return true;
+    if (!pattern.hasMatch(phone)) throw Refuse("手机号格式错误，应该是11位数字，以1开头");
   }
 
   /// 短信验证码：4位数字
-  static bool sms(String sms) {
+  static void sms(String sms) {
     final RegExp pattern = RegExp(r'^\d{4}$');
-    if (!pattern.hasMatch(sms)) return false;
-    return true;
+    if (!pattern.hasMatch(sms)) throw Refuse("短信验证码格式错误，应该是4位数字");
   }
 }
