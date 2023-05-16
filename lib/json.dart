cd049f01b1392251c524413540cfbaa1bb3b5dd9
酸性量子 <lucidquantum@163.com>
2023-05-16 17:09:27 +0800

    支持nullable

--- lib/json.dart
+++ lib/json.dart
@@ -15,15 +15,19 @@ extension JsonParserExtension on String {
 }
 
 extension extractFieldExtension on Json {
-  /// 确保某个key存在指定格式的值
-  ensure<T>(String key) {
+  /// 确保某个key的Type正确
+  void ensure<T>(String key, {bool nullable = false}) {
+    // 允许为空，并且value为空时，跳过检查
+    if (nullable && this[key] == null) return;
+
+    // 其他情况中，都必须有正确格式的value
     if (!containsKey(key)) throw Refuse("Json中缺少$key");
     if (this[key] is! T) throw Refuse("Json中$key的格式不正确，应该为${T.toString()}");
   }
 
   /// 从json中抽出某个具体的数值，如果不是则报错
   T extract<T>(String key) {
-    ensure(key);
+    ensure<T>(key);
     return this[key];
   }
 }
